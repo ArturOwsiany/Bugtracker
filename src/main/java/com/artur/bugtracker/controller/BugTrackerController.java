13c361c31fe72812edaad1f0b24c37e40e3a7f55
Artur Owsiany <o.artur.post@gmail.com>
2023-06-19 20:00:52 +0200

    Thymeleaf config. Preparing pages templates.

---
+++ src/main/java/com/artur/bugtracker/controller/BugTrackerController.java
@@ -0,0 +1,55 @@
+package com.artur.bugtracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class BugTrackerController {
+
+    @GetMapping("/")
+    public String date(Model model) {
+        model.addAttribute("Date", getDate());
+        return "signInPage";
+    }
+
+    public String getDate() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
+
+    @RequestMapping("/requestList")
+    public String requestList() {
+        return "requestList";
+    }
+
+    @RequestMapping("/report")
+    public String raport() {
+        return "report";
+    }
+
+    @RequestMapping("/navbar")
+    public String navbar() {
+        return "navbar";
+    }
+
+    @RequestMapping("/signInPage")
+    public String signInPage() {
+        return "signInPage";
+    }
+
+    @RequestMapping("/mainLayout")
+    public String mainLayout() {
+        return "mainLayout";
+    }
+
+    @RequestMapping("/myAccount")
+    public String myAccount() {
+        return "myAccount";
+    }
+}
